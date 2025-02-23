6957ab55d810fec44d507c35e06463bc35962636
Mykhailo Kharchenko <mixa.kharchenko@gmail.com>
2025-02-23 11:32:50 +0100

    config: add rich presence

--- lua/MeNToSexe/init.lua
+++ lua/MeNToSexe/init.lua
@@ -4,7 +4,7 @@ require("MeNToSexe.set")
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-local delSpacesGroup = augroup('ThePrimeagen', {})
+local delSpacesGroup = augroup('MeNToSexeGroup', {})
 
 autocmd({"BufWritePre"}, {
     group = delSpacesGroup,

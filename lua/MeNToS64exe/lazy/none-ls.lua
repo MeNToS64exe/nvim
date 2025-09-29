252deaa46c607b1b58902039d455258f7d54f321
Mykhailo Kharchenko <mixa.kharchenko@gmail.com>
2025-09-29 16:47:44 +0200

    TODO: find verilog formatter

--- lua/MeNToS64exe/lazy/none-ls.lua
+++ lua/MeNToS64exe/lazy/none-ls.lua
@@ -46,6 +46,9 @@ return {
                         }]],
                     },
                 }),
+
+                -- Verilog/SystemVerilog (verible)
+                -- TODO
             },
         })
     end,
